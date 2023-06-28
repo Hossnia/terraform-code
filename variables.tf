4dd4d867cbc97da70383441e996d745051ada7a9
Hossnia <hossniatraore004@gmail.com>
2023-06-27 22:09:44 -0400

    terraform

---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "my_region" {
+  type    = string
+  default = "us-east-1"
+}
+variable "profile" {
+  default = "default"
+
+}
+variable "topic_name" {
+  default = "sns-dev"
+
+}
+variable "team" {
+  default = "Dev"
+
+}
+
+variable "protocol_type" {
+  default = "email"
+
+
+}
+
+variable "endpoint" {
+  default = "hossniatraore004@gmail.com"
+
+}
